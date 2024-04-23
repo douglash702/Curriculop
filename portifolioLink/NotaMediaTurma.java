5e03aaacdfe22b2ee1d8297f9204aeeedd1e0e3b
Douglas <douglaslimah701@gmail.com>
2024-04-23 19:30:16 -0300

    Adicionando arquivos modificados e não rastreados

--- portifolioLink/NotaMediaTurma.java
+++ portifolioLink/NotaMediaTurma.java
@@ -1,5 +1,49 @@
 package portifolioLink;
 
-public class NotaMediaTurma {
+import java.util.Scanner;
 
+public class NotaMediaTurma {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int alunos = 0;
+		double notas = 0;
+		double total = 0;
+		String conceito = "";
+		while (notas != -1) {
+			System.out.println("Digite a nota do aluno/ou menos -1 para sair");
+			notas = entrada.nextDouble();
+			if (notas <= 10 && notas >= 0) {
+				total += notas;
+				alunos++;
+
+			} else {
+
+				System.out.println("Nota invalida");
+			}
+
+		}
+		double media = total / alunos;
+		System.out.println("A media da turma é " + media);
+
+		if (media >= 9 && media <= 10) {
+			conceito = "A";
+
+		} else if (media >= 7.0) {
+			conceito = "B";
+
+		} else if (media >= 5.0) {
+			conceito = "C";
+
+		} else if (media >= 3.0) {
+			conceito = "D";
+
+		} else {
+			conceito = "E";
+
+		}
+
+		System.out.println("O conceito da turma é: " + conceito);
+		entrada.close();
+
+	}
 }
